3c9943f3691788fee4f6d9e4a4d72396fe646d0b
Will Moore <wmoore@ecotrust.org>
2015-08-19 13:28:42 -0700

    Add Placeholder SVG Map

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,4 +4,26 @@ $(function () {
             top: $('main').offset().top + 10
           }
     });
+
+    MOI.init();
+
 });
+
+
+var MOI = {
+
+    init: function(){
+        MOI.mapToggler();
+    },
+
+    mapToggler: function () {
+        //$('.map-panel').hide();
+
+        $('.map-button').on('click', function(){
+            $(this).parents('.panel')
+                .toggleClass('panel-show-map')
+                .find('.map-holder')
+                .append( $('#main-map') );
+        });
+    }
+}
